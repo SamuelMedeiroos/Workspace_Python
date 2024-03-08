db1f3a5a1a205099d42b10a721e44cb9cb55230b
SamuelMedeiroos <samuel.medeiroos@gmail.com>
2024-03-08 05:54:29 -0300

    Criação de 2 funções do meu programa de tarefas

--- primeiro_projeto/gerenciador.py
+++ primeiro_projeto/gerenciador.py
@@ -1,3 +1,19 @@
+# Funcionalidade de adicionar tarefa
+def adicionar_tarefa(tarefas, nome_tarefa):
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+    return
+
+def ver_tarefas(tarefas):
+    print("\nLista de tarefas:")
+    for indice, tarefa in enumerate(tarefas, start=1):
+            status = "✓" if tarefa["completada"] else " "
+            nome_tarefa = tarefa["tarefa"]
+            print(f"{indice}. [{status}] {nome_tarefa}")
+    return
+
+tarefas = []
 while True:
     print("\nMenu do gerenciador de Lista de Tarefas:")
     print("1. Adicionar tafera")
@@ -9,7 +25,12 @@ while True:
 
     escolha = input("Digite uma opção:")
 
-    if escolha == "6":
+    if escolha == "1":
+        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+        adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "2":
+        ver_tarefas(tarefas)
+    elif escolha == "6":
         break
 
 print("Programa finalizado")
